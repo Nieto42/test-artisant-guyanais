24cda2597c8a38841a9e1c57cb6f39782c85270f
bruno3du <bruno.3duardo@hotmail.com>
2022-06-07 20:31:27 -0300

    some changes

--- components/Form.js
+++ components/Form.js
@@ -1,74 +1,89 @@
-import React, { useState, useEffect } from "react";
-import Cards from "./Cards";
+/** @format */
+
+import React, { useState, useEffect } from 'react';
+import Cards from './Cards';
 
 export default function Form({ artisans }) {
-  const [proData, setProData] = useState([]);
-  console.log(artisans);
-  useEffect(() => {
-    setProData(artisans);
-  }, []);
+	const [proData, setProData] = useState([]);
 
-  const [value, setValue] = useState();
+	useEffect(() => {
+		setProData(artisans);
+	}, []);
 
-  const search = (e) => {
-    e.preventDefault();
-    if (value === "") {
-      setProData(artisans);
-    } else {
-      var usuariosFiltrados = artisans.filter((usuario) => {
-        const newData = Object.keys(usuario).map((item) => {
-          if (typeof usuario[item] !== "string") return;
+	const [value, setValue] = useState();
 
-          if (value.toLowerCase() == usuario[item].toLowerCase()) {
-            return true;
-          } else {
-            return false;
-          }
-        });
-        console.log(newData);
-        return newData.id === usuario.id;
-      });
-      setProData(usuariosFiltrados);
-    }
-  };
+	const search = (e) => {
+		e.preventDefault();
+		if (value === '') {
+			setProData(artisans);
+		} else {
+			var usuariosFiltrados = artisans.filter((usuario) => {
+				const newData = Object.keys(usuario).map((item) => {
+					if (typeof usuario[item] === 'number') {
+						return;
+					}
 
-  return (
-    <div className="form-component">
-      <div className="form-container">
-        <form onSubmit={search}>
-          <input
-            type="search"
-            name="data-art"
-            id="seach-input"
-            placeholder="Entrez un besoin"
-            onChange={(e) => setValue(e.target.value)}
-            value={value}
-          />
-          <button type="submit" className="btn">
-            Search
-          </button>
-        </form>
-        {/* bouton pour trier du meilleur au pire */}
-        <div className="btn-sort-container">
-          <button className="btn-sort" id="goodTobad">
-            Top <span>→</span>
-          </button>
-          <button className="btn-sort" id="badToGood">
-            Flop <span>→</span>
-          </button>
-        </div>
-      </div>
-      <div className="result">
-        {proData.slice(0, 12).map((info) => (
-          <Cards key={info.id} info={info}></Cards>
-        ))}
-      </div>
-    </div>
-  );
+					if (typeof usuario[item] === 'string') {
+						if (usuario[item].toLowerCase().includes(value.toLowerCase())) {
+							return item;
+						} else {
+							return false;
+						}
+					}
+
+					if (typeof usuario[item] === 'object' && usuario[item] !== null) {
+						return usuario[item].some((ele) => {
+							return ele.toLowerCase().includes(value.toLowerCase());
+						});
+					} else {
+						return false;
+					}
+				});
+				const filter = newData.some((ele) => ele);
+
+				return filter;
+			});
+			setProData(usuariosFiltrados);
+		}
+	};
+
+	return (
+		<div className='form-component'>
+			<div className='form-container'>
+				<form onSubmit={search}>
+					<input
+						type='search'
+						name='data-art'
+						id='seach-input'
+						placeholder='Entrez un besoin'
+						onChange={(e) => setValue(e.target.value)}
+						value={value}
+					/>
+					<button type='submit' className='btn'>
+						Search
+					</button>
+				</form>
+				{/* bouton pour trier du meilleur au pire */}
+				<div className='btn-sort-container'>
+					<button className='btn-sort' id='goodTobad'>
+						Top <span>→</span>
+					</button>
+					<button className='btn-sort' id='badToGood'>
+						Flop <span>→</span>
+					</button>
+				</div>
+			</div>
+			<div className='result'>
+				{proData.slice(0, 12).map((info) => (
+					<Cards key={info.id} info={info}></Cards>
+				))}
+			</div>
+		</div>
+	);
 }
 
 {
-  /* {artisans.map((artisan, pro) => (
+	/* {artisans.map((artisan, pro) => (
  
     <div className="test">
 
